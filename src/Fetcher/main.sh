a9281914fab7bb1c5bc3e6b479b931c35efd2541
Nezred ✨ <101522353+nezred@users.noreply.github.com>
2024-11-20 00:10:26 +0100

    feat: añade dockerfile y programas básicos vacíos para el fetcher y rest_api
    lo único que falta es el esqueleto del frontend,
    y discutir que hacemos con el fetcher, ha quedado
    algo raro.

---
+++ src/Fetcher/main.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+# maybe datasetscan is not needed, since it will only run once, it shouldnt be too wasteful to program it as a separate container
+python datasetscan.py &
+fastapi run imgfetcher.py
