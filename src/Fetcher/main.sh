241ea0a7c54f4e52d9c3a256438a58b125e78c2e
Nezred ✨ <101522353+nezred@users.noreply.github.com>
2024-11-20 19:19:31 +0100

    feat: add fetcher skeleton & docker compose now functional
    i also modified some things to make the docker-compose work

--- src/Fetcher/main.sh
+++ src/Fetcher/main.sh
@@ -2,4 +2,4 @@
 
 # maybe datasetscan is not needed, since it will only run once, it shouldnt be too wasteful to program it as a separate container
 python datasetscan.py &
-fastapi run imgfetcher.py
+fastapi run /app/imgfetcher.py
