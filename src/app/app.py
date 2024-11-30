8504c9be7ea7684f271845ffdbf0d84faad2fdf3
Lunao01 <FcoJavier.Luna@alu.uclm.es>
2024-11-30 20:09:41 +0100

    add @app.route('/menu') and create user.py

--- src/app/app.py
+++ src/app/app.py
@@ -3,6 +3,8 @@ from config import config # archivo config.py
 
 app = Flask(__name__)
 
+# db = MySQL(app)
+
 # Nada más cargar la ruta raíz te reenvia a /login
 @app.route('/')
 def index():
@@ -14,14 +16,18 @@ def login():
     if request.method == 'POST':
         print(request.form['username']) # username del formulario
         print(request.form['password']) # password del formulario
-        return render_template('menu/menu.html')
+        return redirect(url_for('menu'))
 
     else: # Si es GET se renderiza la página login.html
         return render_template('auth/login.html') # ruta de la plantilla html index
+
+@app.route('/menu')
+def menu():
+    return render_template('menu/menu.html')
 
 
 if __name__ == '__main__':
     app.config.from_object(config['development']) # usar configuración del diccionario definido en config.py
     app.run()
 
-    # Se ha guardado :)
+    # Se ha guardado :) 2
--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,13 @@
 # Archivo para configurar app.py
-class DevelopmentConfig():
+class Config:
+    SECRET_KEY = 'S8!}.Nz=_ba?@v5QPdcZ\^5a,Y>],F'
+
+class DevelopmentConfig(Config):
     DEBUG = True # modo debug para poder aplicar cambios con el servidor lanzado
+    MYSQL_HOST = 'localhost' # host
+    MYSQL_USER = 'root' # usuario 
+    MYSQL_PASSWORD = '123456' # password
+    MYSQL_DB = 'poke_quest' # nombre de la base de datos
 
 config = {
     'development': DevelopmentConfig
