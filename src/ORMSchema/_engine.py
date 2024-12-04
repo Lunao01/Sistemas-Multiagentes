5cb0ae16d845560fb461b4f2d84036514fab0cf8
Nezred ✨ <101522353+nezred@users.noreply.github.com>
2024-12-04 20:19:23 +0100

    Refactor database connection string to use containerized database

--- src/ORMSchema/_engine.py
+++ src/ORMSchema/_engine.py
@@ -7,6 +7,6 @@ DB_PASSWORD=getenv("DB_PASSWORD")
 DB_USER=getenv('DB_USER') if getenv('DB_USER') is not None else 'postgres' # default user
 
 assert DB_PASSWORD is not None, "DB_PASSWORD is not set"
-engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/pokemon")
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@db:5432/pokemon")
 # engine = create_engine('sqlite:///pokemon.db')
 del DB_PASSWORD,DB_USER
