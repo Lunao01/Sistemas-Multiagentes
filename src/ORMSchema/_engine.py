e956513235f83d3d019ef5ae7aff3e73ea445eea
Nezred ✨ <101522353+nezred@users.noreply.github.com>
2024-12-14 00:20:02 +0100

    fix server address issue

--- src/ORMSchema/_engine.py
+++ src/ORMSchema/_engine.py
@@ -1,7 +1,6 @@
 from os import getenv
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
 
 assert load_dotenv(), "Failed to load .env file"
 
@@ -11,8 +10,6 @@ DB_PASSWORD=getenv("DB_PASSWORD")
 DB_USER=getenv('DB_USER') if getenv('DB_USER') is not None else 'postgres' # default user
 
 assert DB_PASSWORD is not None, "DB_PASSWORD is not set"
-engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@db:5432/pokemon")
-if not database_exists(engine.url):
-    create_database(engine.url)
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@db:5432",echo=True)
 # engine = create_engine('sqlite:///pokemon.db')
 del DB_PASSWORD,DB_USER
--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,7 @@
 # Archivo para configurar app.py
 class DevelopmentConfig():
     DEBUG = True # modo debug para poder aplicar cambios con el servidor lanzado
+    SERVER_NAME='0.0.0.0:5000'
 
 config = {
     'development': DevelopmentConfig
